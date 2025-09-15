e6eb4e31a4815736f1a17f081af962573c00ac5d
rohit20001221 <rohit20001221@gmail.com>
2025-09-16 01:38:32 +0530

    added logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
 import type { FC } from "react";
+import logo from "@/assets/kitty.png";
 
 export const Header: FC = () => {
   return (
-    <div className="p-2 h-[var(--header-height)] flex items-center shadow-sm">
+    <div className="p-2 h-[var(--header-height)] flex items-center shadow-sm gap-2">
+      <img src={logo} width={40} height={40} />
       <span className="text-xl">Kitten</span>
     </div>
   );
