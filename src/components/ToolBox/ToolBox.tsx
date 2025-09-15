800edb80cb656550f458f5108df1695f966db1bc
rohit20001221 <rohit20001221@gmail.com>
2025-09-16 01:10:40 +0530

    layout and react router

--- src/components/ToolBox/ToolBox.tsx
+++ src/components/ToolBox/ToolBox.tsx
@@ -1,17 +1,26 @@
 import type { FC } from "react";
 import { Button } from "@/components/ui/button";
 import { Layers, Image } from "lucide-react";
+import { NavLink, Outlet } from "react-router";
 
 export const ToolBox: FC = () => {
   return (
-    <div className="w-[480px] h-full border-r">
+    <div className="w-[400px] h-full border-r">
       <div className="p-2 border-b flex gap-1 items-center">
-        <Button variant={"secondary"} size={"icon"} className="size-8">
-          <Layers />
-        </Button>
-        <Button variant={"secondary"} size={"icon"} className="size-8">
-          <Image />
-        </Button>
+        <NavLink to={"/layers"}>
+          <Button variant={"secondary"} size={"icon"} className="size-8">
+            <Layers />
+          </Button>
+        </NavLink>
+
+        <NavLink to={"/sprites"}>
+          <Button variant={"secondary"} size={"icon"} className="size-8">
+            <Image />
+          </Button>
+        </NavLink>
+      </div>
+      <div className="p-1 overflow-hidden">
+        <Outlet />
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<App />}>
+          <Route path="" element={<Navigate to={"/layers"} />} />
+          <Route path="/layers" element={<div>layers</div>} />
+          <Route path="/sprites" element={<div>sprites</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
