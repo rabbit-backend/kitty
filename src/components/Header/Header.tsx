b444ece0b64010f505b1ef969b237c1b3f7bbb45
rohit20001221 <rohit20001221@gmail.com>
2025-09-16 01:00:47 +0530

    ..

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import type { FC } from "react";
 export const Header: FC = () => {
   return (
     <div className="p-2 h-[var(--header-height)] flex items-center shadow-sm">
-      <span className="text-xl">Kitty</span>
+      <span className="text-xl">Kitten</span>
     </div>
   );
 };
